3ac19e84388d855e804cb9714ddd7fd3d6fe1271
basilbseiso <bb1026@georgetown.edu>
2022-04-12 13:59:50 -0400

    Bug Fixes

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -26,14 +26,17 @@ def another():
 
 @home_routes.route("/hello")
 def hello_world():
-    print("HELLO...", dict(request.args))
-
-    # go check the URL params for one called "name", and use it if possible
-    # if no "name" parameter is specified, use a default value
-    # NOTE: `request.args` is dict-like, so below we're using the dictionary's `get()` method,
-    # ... which will return None instead of throwing an error if key is not present
-    # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
-    name = request.args.get("name") or "World"
+    print("HELLO...")
+    
+    # if the request contains url params, for example a request to "/hello?name=Harper"
+    # the request object's args property will hold the values in a dictionary-like structure
+    url_params = dict(request.args)
+    print("URL PARAMS:", url_params) #> can be empty like {} or full of params like {"name":"Harper"}
+    
+    # get a specific key called "name" if available, otherwise use some specified default value
+    # see also: https://www.w3schools.com/python/ref_dictionary_get.asp
+    name = url_params.get("name") or "World"
+    
     message = f"Hello, {name}!"
     return message
     #return render_template("hello.html", message=message)
